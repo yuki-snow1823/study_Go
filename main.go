4a1b8b934d705dc8750596f807bc8b3298e98565
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2021-11-03 08:29:26 +0900

    init

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	// UTFの文字コードで変換されているから、任意のものは出ない
+	var number int = 1234
+	var numberStr string = string(number)
+	fmt.Println(numberStr)
+
+	var i int
+	var s string = "123"
+	i, _ = strconv.Atoi(s)
+	fmt.Println(i) // 123が出る
+
+	num := 0
+	for n := 0; n < 100000000; n++ {
+		num += n
+	}
+	fmt.Println(num)
+}
---
+++ time_test.rb
@@ -0,0 +1,10 @@
+i = 0
+num = 0
+100000000.times do
+  num += i
+  i += 1
+end
+
+puts num
+
+# 3.1s
